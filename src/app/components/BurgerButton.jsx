a477bc1ddff91a6f8e503355d805451b94f5e378
ellaggon <ellaggon@gmail.com>
2025-02-04 22:30:53 -0300

    feat(BurgerButton): create burger button & add function

---
+++ src/app/components/BurgerButton.jsx
@@ -0,0 +1,13 @@
+"use client"
+
+export const BurgerButton = ({clicked, handleClick}) => {
+    return (
+        <div
+        onClick={handleClick}
+        className={`w-9 h-7 relative cursor-pointer inline-block ${clicked ? "open" : ""}`}>
+            <span className={`absolute w-full h-1 bg-white rounded transition-transform duration-500 ${clicked ? "rotate-45 top-[13px]" : "top-0"}`}></span>
+            <span className={`absolute w-full h-1 bg-white rounded transition-opacity duration-500 ${clicked ? "opacity-0" : "top-[13px]"}`}></span>
+            <span className={`absolute w-full h-1 bg-white rounded transition-transform duration-500 ${clicked ? "-rotate-45 top-[13px]" : "bottom-0"}`}></span>
+        </div>
+    );
+};
--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,6 +1,8 @@
 "use client"
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { BurgerButton } from "./BurgerButton";
+import { useState } from "react";
 
 export const Header = () => {
     const pathname = usePathname()
@@ -15,12 +17,14 @@ export const Header = () => {
 
     const active = headerNav.find(e => e.path === pathname)
 
+    const [ isBurgerOpen, setIsBurgerOpen ] = useState(false)
+
     return (
         <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50 bg-opacity-25 backdrop-blur-md border-b border-white/20">
             <div className="container flex items-center justify-between h-16 px-4 md:px-6">
                 {/* burger button */}
                 <div className="md:hidden">
-                    <h2>Header</h2>
+                    <BurgerButton clicked={isBurgerOpen} handleClick={() => setIsBurgerOpen(!isBurgerOpen)} />
                 </div>
 
                 <h1 className={`text-2xl font-bold text-main md:text-4xl`}>
