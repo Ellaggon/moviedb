2829e01fae26064a576551bee4b9cd178ca850c0
ellaggon <ellaggon@gmail.com>
2025-02-07 12:25:20 -0300

    feat(SearchInput): create a searchInput for nav when the client is in full screen

--- src/app/components/header/SearchInput.tsx
+++ src/app/components/header/SearchInput.tsx
@@ -1,8 +1,54 @@
+"use client"
 
-export const SearchInput = () => {
+import { useRouter } from "next/navigation";
+import { useRef, useState } from "react";
+import { BiSearch } from "react-icons/bi";
+
+export default function SearchInput() {
+    const inputRef = useRef<HTMLInputElement | null>(null)
+    const router = useRouter()
+
+    const [text, setText] = useState("")
+    const [error, setError] = useState(false)
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setText(e.target.value) 
+        setError(false)
+    }
+
+    const validate = (value: string) => {
+        if(!value.trim()){
+            inputRef.current?.focus()
+            setError(true)
+            return false
+        }
+        setError(false)
+        return true
+    }
+
+    const onSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+        if(validate(text)) {
+            router.push(`/search/name=${encodeURIComponent(text)}`)
+        }
+        setText("")
+    }
     return (
-        <div>
-            {/* <input type="text" placeholder="Search" className="w-96 h-12 px-4 py-2 bg-white border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-main focus:ring-opacity-50" /> */}
-        </div>
+        <article className={`hidden md:flex items-center p-2 inputs border-2 rounded-lg ${error ? "border-red-500" : "border-gray-700"}`}>
+            <form className="flex items-center gap-2 w-full" onSubmit={onSubmit}>
+                <input
+                    ref={inputRef}
+                    type="text"
+                    value={text}
+                    onChange={onChange}
+                    placeholder="Search"
+                    className="w-[110px] bg-transparent rounded-lg outline-none text-sm tracking-wide placeholder-white" />
+                <button 
+                    type="submit" 
+                    className="p-1">
+                    <BiSearch className="text-white"/>
+                </button>
+            </form>
+        </article>
     );
 }
