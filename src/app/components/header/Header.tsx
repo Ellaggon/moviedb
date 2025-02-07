8e273b3b0a9c96e13dc94ab3d0a94574113fc67f
ellaggon <ellaggon@gmail.com>
2025-02-07 12:27:28 -0300

    refactor(header): improve semantic code in header

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,62 +1,81 @@
-"use client"
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+"use client";
 
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { BurgerButton } from "./BurgerButton";
-// import { SearchInput } from "./SearchInput";
+import Link from "next/link";
 import SearchButton from "./SearchButton";
+import SearchInput from "./SearchInput";
 
-interface NavItem {
-    display: string;
-    path: string
-}
+// import { useTranslation } from "react-i18next";
+// import LanguageToggle from "../language-toggle/LanguageToggle";
 
-export const Header = () => {
-    const pathname = usePathname()
+export default function Header() {
+    const pathname = usePathname();
+    //   const { t } = useTranslation("global");
 
-    const headerNav: NavItem[] = [
+    const headerNav = [
         { display: "home", path: "/" },
-        {display: "trending" ,path: "/treding"},
-        {display: "categories", path: "/categories"},
-        {display: "popular", path: "/popular"},
-        {display: "upcoming", path: "/Upcomig"}
-    ]
+        { display: "trending", path: "/trending" },
+        { display: "categories", path: "/categories" },
+        { display: "popular", path: "/popular" },
+        { display: "upcoming", path: "/upcoming" },
+    ];
 
-    const activeIndex = headerNav.findIndex(e => e.path === pathname)
+    const active = headerNav.findIndex((e) => e.path === pathname);
 
-    const [ isBurgerOpen, setIsBurgerOpen ] = useState(false)
-    const [ isSearchOpen, setIsSearchOpen ] = useState(false)
+    const [clickedBurger, setClickedBurger] = useState(false);
+    const [clickedSearcher, setClickedSearcher] = useState(false);
 
     return (
-        <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50 bg-opacity-25 backdrop-blur-md border-b border-white/20">
-            <div className="container flex items-center justify-between h-16 px-4 md:px-6">
+        <header className="fixed relative top-0 left-0 w-full bg-black/30 backdrop-blur-md border-b border-white/20 z-50">
+            <ul className="flex container items-center justify-between h-16 px-5">
+
                 {/* burger button */}
-                <div className="md:hidden mt-2">
-                    <BurgerButton clicked={isBurgerOpen} handleClick={() => setIsBurgerOpen(!isBurgerOpen)} />
-                </div>
+                <li className="block md:hidden">
+                    <BurgerButton
+                        clicked={clickedBurger}
+                        handleClick={() => setClickedBurger(!clickedBurger)} />
+                </li>
 
-                <h1 className={`fixed right-[35%] text-2xl font-bold md:text-4xl transition-all ${isSearchOpen? "hidden" : "flex"}`} >
-                    <Link href="/">MOVIEAPP</Link>
-                </h1>
+                {/* Títle */}
+                <li className={`${clickedSearcher ? "hidden md:block" : ""}`}>
+                    <Link href="/" className="text-xl ml-6 font-bold text-red-500">
+                        MOVIEAPP
+                    </Link>
+                </li>
 
-                {/* menu */}
-                <nav className={`absolute top-16 left-0 w-full bg-black md:static md:w-auto md:flex md:gap-16 transition-transform md:translate-x-0`}>
-                    <ul className="flex flex-col md:flex-row md:gap-6">
-                        {
-                            headerNav.map((item, index) => (
-                                <li key={index} className={`text-white px-4 py-2 md:px:0 md:py-0 ${index === activeIndex ? "border-b-2 border-main" : "" }`}> 
-                                    <Link href={item.path}>{item.display}</Link>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </nav>
+                {/* navigation menu */}
+                <li
+                    className={`absolute md:static md:z-50 top-16 left-0 w-full md:w-auto bg-black md:bg-transparent p-5 md:p-0 flex flex-col md:flex-row items-center gap-5 transition-all -z-10 ${clickedBurger ? "translate-y-0" : "-translate-y-full md:translate-y-0"
+                        }`}
+                >
+                    {headerNav.map((e, i) => (
+                        <Link
+                            key={i}
+                            href={e.path}
+                            className={`text-white text-sm md:text-lg transition ${i === active ? "border-b-2 border-red-500" : "hover:border-b-2 hover:border-white"
+                                }`}
+                        >
+                            {e.display}
+                        </Link>
+                    ))}
+                    <div className="w-full h-16"></div>
+                </li>
 
-                <div className="block md:hidden">
-                    <SearchButton clicked={isSearchOpen} handleClick={() => setIsSearchOpen(!isSearchOpen)} />
-                </div>
-            </div>
+                {/* config controls */}
+                <li className="flex items-center gap-3">
+                    {/* <LanguageToggle clickedBurger={clickedBurger} /> */}
+                    <SearchInput />
+                </li>
+
+                {/* Search button */}
+                <li className="block md:hidden">
+                    <SearchButton
+                        clicked={clickedSearcher}
+                        handleClick={() => setClickedSearcher(!clickedSearcher)} />
+                </li>
+            </ul>
         </header>
     );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Header } from "./components/header/Header";
+import Header from "./components/header/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
