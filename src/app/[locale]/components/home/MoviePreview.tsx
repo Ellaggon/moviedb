86be8e2feac2a46c67040f4540b59df5b9b700a3
ellaggon <ellaggon@gmail.com>
2025-02-15 21:01:30 -0300

    refactor(index page): create Movie preview component & add to home page

---
+++ src/app/[locale]/components/home/MoviePreview.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link"
+import MovieSwipe from "./MoviesSwipe"
+import { OutlineButton } from "../button"
+import { getTranslations } from "next-intl/server"
+
+interface MoviePreviewProps {
+    title: string,
+    category: string,
+    redirect: string,
+    id?: number
+}
+
+export async function MoviePreview({ title, category, redirect, id }: MoviePreviewProps) {
+    const t = await getTranslations("lang")
+    return (
+        <section className="mb-10">
+            <div className="max-w-screen-lg mx-auto">
+                <div className="flex items-center justify-between mb-5">
+                    <h2 className="text-2xl sm:text-3xl font-bold">{title}</h2>
+                    {
+                        category !== "similar" && (
+                            <Link href={redirect}>
+                                <OutlineButton className="px-4 py-2 border border-white rounded text-white text-sm hover:bg-white hover:text-black transition">
+                                    {t("seeAll")}
+                                </OutlineButton>
+                            </Link>
+                        )
+                    }
+                </div>
+                <MovieSwipe category={category} id={id ?? 0} />
+            </div>
+        </section>
+    )
+}
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,18 @@
 
-import HomeHero from "@/[locale]/components/home/HomeHero";
 import { getTranslations } from "next-intl/server";
-
+import HomeHero from "./components/home/HomeHero";
+import {MoviePreview} from "./components/home/MoviePreview";
 
 export default async function home() {
   const t = await getTranslations("lang")
   return (
     <>
-      <HomeHero />
       <div className="p-6 text-center">
         <h1 className="text-2xl font-bold mt-4">{t('home')}</h1>
+        <HomeHero />
+        <MoviePreview title={t("trending")} category="trending" redirect={`${t("lang")}/trending`}/>
+        <MoviePreview title={t("popular")} category="popular" redirect={`${t("lang")}/popular`}/>
+        <MoviePreview title={t("upcoming")} category="upcoming" redirect={`${t("lang")}/upcoming`}/>
       </div>
     </>
   )
