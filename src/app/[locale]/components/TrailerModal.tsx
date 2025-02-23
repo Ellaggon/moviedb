936f58cdbedd32371d1b68fc1ac794c6a3a584fb
ellaggon <ellaggon@gmail.com>
2025-02-23 20:23:00 -0300

    feat(trailerModal): create trailer modal component for home hero

---
+++ src/app/[locale]/components/TrailerModal.tsx
@@ -0,0 +1,13 @@
+"use client"
+export const TrailerModal = () => { 
+    try {
+        
+    } catch (e) {
+        console.error("Error:", e)
+    }
+    return (
+        <article>
+            <h1>Trailer Modal</h1>
+        </article> 
+    )
+}
