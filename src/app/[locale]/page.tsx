54ab0933c5c727df58c950266dce00f7b8cd675d
ellaggon <ellaggon@gmail.com>
2025-03-01 22:28:28 -0300

    refactor(index): Improve UI

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -9,7 +9,7 @@ export default async function home() {
     <>
       <div className="text-center">
         <HomeHero />
-        <div className="px-6">
+        <div className="p-6">
         <MoviePreview title={t("trending")} category="trending" redirect={`${t("lang")}/trending`}/>
         <MoviePreview title={t("popular")} category="popular" redirect={`${t("lang")}/popular`}/>
         <MoviePreview title={t("upcoming")} category="upcoming" redirect={`${t("lang")}/upcoming`}/>
