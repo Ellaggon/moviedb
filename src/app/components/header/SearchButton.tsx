1ea4a3ca5f959ece1b1bfc0b781f0cc879ff7e67
ellaggon <ellaggon@gmail.com>
2025-03-06 20:56:47 -0300

    refactor(searchButton): add useTranslation to url in searchButton

--- src/app/components/header/SearchButton.tsx
+++ src/app/components/header/SearchButton.tsx
@@ -3,10 +3,12 @@
 import { useRef, useState } from "react";
 import { BiSearch } from "react-icons/bi"
 import { useRouter } from "next/navigation";
+import { useTranslations } from "use-intl";
 
 export default function SearchButton({ clicked, handleClick }: { clicked: boolean; handleClick: () => void }) {
   const inputRef = useRef<HTMLInputElement>(null)
   const router = useRouter()
+  const t  = useTranslations("lang")
 
   const [text, setText] = useState("")
   const [error, setError] = useState(false)
@@ -29,14 +31,28 @@ export default function SearchButton({ clicked, handleClick }: { clicked: boolea
   const onSubmit = (e: React.FormEvent) => {
     e.preventDefault()
     if (validate(text)) {
-      router.push(`/search/name=${encodeURIComponent(text)}`)
+      router.push(`/${t("lang")}/search?name=${encodeURIComponent(text)}`)
       setText("")
     }
   }
   return (
-    <div className={`transition-all ${clicked ? "max-w-md border-2 border-white rounded-lg ml-auto" : ""} ${error ? "border-red-500" : ""}`}>
+    <div className={`transition-all ${
+      clicked ? "max-w-md border-2 border-white rounded-lg ml-auto" : ""
+      } ${
+        error ? "border-red-500" : ""
+        }`}>
       <form onSubmit={onSubmit} className={`flex items-center gap-2 transition-all `}>
-        <input ref={inputRef} type="text" value={text} onChange={onChange} placeholder="Search" className={`bg-transparent outline-none text-white transition-all ${clicked ? "w-full px-3 py-1 opacity-100" : "w-0 opacity-0"} ${error ? "border-red-500" : ""}`} />
+        <input 
+          ref={inputRef} 
+          type="text" 
+          value={text} 
+          onChange={onChange} 
+          placeholder="Search" 
+          className={`bg-transparent outline-none text-white transition-all ${
+            clicked ? "w-full px-3 py-1 opacity-100" : "w-0 opacity-0"
+            } ${
+              error ? "border-red-500" : ""
+              }`} />
 
         <button onClick={handleClick} type="submit" className="bg-trasparent border-none transition-all">
           <BiSearch className="text-white text-4xl hover:text-blue-500" />
