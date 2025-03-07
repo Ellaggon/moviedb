5022d53dd32520a3df8086eeea3d01ed4d5588bd
ellaggon <ellaggon@gmail.com>
2025-03-06 21:00:36 -0300

    reafactor(genreMovies): add pagination to categories page

--- src/app/components/categories/GenreMovies.tsx
+++ src/app/components/categories/GenreMovies.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 import { tmdbApi } from "src/api/tmdbApi"
 import MovieCard from "../MovieCard"
 import { useTranslations } from "use-intl"
+import { Pagination } from "../Pagination"
 
 type Movie = {
     id: number,
@@ -16,28 +17,39 @@ interface GenreMovieProps {
 }
 
 export const GenreMovies = ({ selectedGenres }: GenreMovieProps) => {
-    const [listMovies, setListMovies] = useState<Movie[]>([])
     const t = useTranslations("lang")
+
+    const [listMovies, setListMovies] = useState<Movie[]>([])
+    const [totalPage, setTotalPage] = useState(0)
+    const [currentPage, setCurrentPage] = useState(0)
 
     useEffect(() => {
         const getList = async () => {
             const params = {
-                page: 1,
+                page: currentPage || 1,
                 language: `${ t("lang") }`,
             }
             try {
                 const { data } = await tmdbApi.getMoviesByCategory(selectedGenres, params)
-                const movies = data.results
-                setListMovies(movies)
+                const { results, total_pages, page} = data
+
+                setListMovies(results)
+                setTotalPage(total_pages)
+                setCurrentPage(page)
             } catch (e) {
-                console.error("Error:", e)
+                console.error("Error fetching list movies :", e)
             }
         }
         getList()
-    }, [selectedGenres])
+    }, [selectedGenres, currentPage])
+
+    const handlePages = (newPage: number) => {
+        setCurrentPage(newPage)
+        window.scrollTo({top: 0, behavior: "smooth"})
+    }
 
     return (
-        <section className="flex justify-center">
+        <section className="flex flex-col justify-center">
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {
                     listMovies.map((movie) => (
@@ -45,6 +57,7 @@ export const GenreMovies = ({ selectedGenres }: GenreMovieProps) => {
                     ))
                 }
             </div>
+                <Pagination totalPages={totalPage} category="categories" currentPage={currentPage} sendPage={handlePages}/>
         </section>
     )
 }
