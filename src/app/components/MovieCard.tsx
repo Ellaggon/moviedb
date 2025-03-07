2f5d22638e602599e5ff22dd2f6249336f2e066e
ellaggon <ellaggon@gmail.com>
2025-03-06 21:04:40 -0300

    refactor(movieCard): fix UI, center the card

--- src/app/components/MovieCard.tsx
+++ src/app/components/MovieCard.tsx
@@ -21,7 +21,7 @@ export default function MovieCard({ item }: itemMovie) {
     const link = `/${t("lang")}/movie/${item.id}`
     const bg = apiConfig.w500Image(item.poster_path || item.backdrop_path || "")
     return (
-        <Link href={link} className="block mb-2">
+        <Link href={link} className="mb-2 flex flex-col justify-center items-center w-full">
             <div style={{ backgroundImage: `url(${bg})`}}
                 className="relative w-40 h-60 md:w-56 md:h-80 bg-cover bg-center rounded-lg transition-transform hover:scale-105">
                 <button className="absolute inset-0 flex justify-center items-center bg-black bg-opacity-60 opacity-0 hover:opacity-100 transition-opacity rounded-lg">
