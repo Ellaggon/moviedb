e1ba8816b126ee08e6bd6218c6b8e5025e0b355c
ellaggon <ellaggon@gmail.com>
2025-03-08 07:14:45 -0300

    refactor(pagination): add disabled in buttons

--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -4,9 +4,9 @@ import { useRouter } from "next/navigation"
 import { useTranslations } from "use-intl"
 
 type PaginationProps = {
-    totalPages: number,
-    category: string,
-    currentPage: number,
+    totalPages: number
+    category: string
+    currentPage: number
     query?: string | string[] |undefined
     sendPage?: (page: number) => void
 }
@@ -38,25 +38,30 @@ export const Pagination = ({category, totalPages, currentPage, query, sendPage}:
         <div className="hidden lg:flex justify-center space-x-2 mt-6 pt-10">
             <button 
                 onClick={() => sendPage ? sendPage(currentPage - 1) : createPageUrl(currentPage - 1)}
+                disabled={ currentPage === 1 }
                 className={`px-4 py-2 border rounded-lg ${
                     currentPage === 1 ? "bg-black-900 text-gray-900 cursor-not-allowed" : "bg-gray-800 text-white"
                 }`}>
                 ← Anterior
             </button>
             {
-                Array.from({length: 5}, ((_, i) => startPage + i)).map(page => (
+                Array.from({length: 10}, ((_, i) => startPage + i)).map(page => (
                     <button 
                         key={page}
                         onClick={() => sendPage ? sendPage(page) : createPageUrl(page)}
+                        disabled={page > totalPages}
                         className={`px-4 py-2 border rounded-lg font-bold hover:bg-red-900 ${
                             page === currentPage ? "bg-red-700 text-white" : "bg-black"
-                        }`}>
-                        { page }
+                        }${
+                            page > totalPages ? "cursor-not-allowed bg-black-900 text-gray-900 hover:bg-black": ""
+                            }`}>
+                            { page }
                     </button>
                 ))
             }
             <button 
                 onClick={() => sendPage ? sendPage(currentPage + 1) : createPageUrl(currentPage + 1)}
+                disabled={currentPage >= totalPages}
                 className={`px-4 py-2 border rounded-lg ${
                     currentPage === totalPages ? "bg-black-900 text-gray-900 cursor-not-allowed" : "bg-gray-800 text-white"
                 }`}>
