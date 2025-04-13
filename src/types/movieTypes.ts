71db3ae1b8734abcb5db6a51bb79a317c70a1132
ellaggon <ellaggon@gmail.com>
2025-04-13 19:38:50 -0400

    feat(types): create an archive for Movie detail

--- src/types/movieTypes.ts
+++ src/types/movieTypes.ts
@@ -2,3 +2,12 @@
 export type SearchParams = {
     searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }
+
+export type MovieDetail = {
+    id: number
+    title: string
+    name?: string
+    poster_path: string
+    backdrop_path?: string
+    overview?: string
+}
